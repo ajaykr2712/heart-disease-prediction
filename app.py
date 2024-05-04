b175b1b05df0bab703ad4ec3968572ff78118998
Ajay Kumar <112938234+ajaykr2712@users.noreply.github.com>
2024-05-04 22:50:33 +0530

    Update app.py
    the rephrased code with variable names changed:

--- app.py
+++ app.py
@@ -2,40 +2,39 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-filename = 'heart-disease-prediction-kmeans-model.pkl'
-model = pickle.load(open(filename, 'rb'))
+model_filename = 'heart-disease-prediction-kmeans-model.pkl'
+loaded_model = pickle.load(open(model_filename, 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-	return render_template('main.html')
-
+def home_page():
+	return render_template('index.html')
 
 @app.route('/predict', methods=['GET','POST'])
-def predict():
+def make_prediction():
     if request.method == 'POST':
 
-        age = int(request.form['age'])
-        sex = request.form.get('sex')
-        cp = request.form.get('cp')
-        trestbps = int(request.form['trestbps'])
-        chol = int(request.form['chol'])
-        fbs = request.form.get('fbs')
-        restecg = int(request.form['restecg'])
-        thalach = int(request.form['thalach'])
-        exang = request.form.get('exang')
-        oldpeak = float(request.form['oldpeak'])
-        slope = request.form.get('slope')
-        ca = int(request.form['ca'])
-        thal = request.form.get('thal')
+        patient_age = int(request.form['patient_age'])
+        patient_sex = request.form.get('patient_sex')
+        chest_pain_type = request.form.get('chest_pain_type')
+        resting_blood_pressure = int(request.form['resting_blood_pressure'])
+        serum_cholesterol = int(request.form['serum_cholesterol'])
+        fasting_blood_sugar = request.form.get('fasting_blood_sugar')
+        resting_ecg = int(request.form['resting_ecg'])
+        max_heart_rate = int(request.form['max_heart_rate'])
+        exercise_induced_angina = request.form.get('exercise_induced_angina')
+        st_depression = float(request.form['st_depression'])
+        st_slope = request.form.get('st_slope')
+        num_major_vessels = int(request.form['num_major_vessels'])
+        thalassemia = request.form.get('thalassemia')
         
-        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
-        my_prediction = model.predict(data)
+        input_data = np.array([[patient_age, patient_sex, chest_pain_type, resting_blood_pressure, 
+                                serum_cholesterol, fasting_blood_sugar, resting_ecg, max_heart_rate, 
+                                exercise_induced_angina, st_depression, st_slope, num_major_vessels, thalassemia]])
+        prediction_result = loaded_model.predict(input_data)
         
-        return render_template('result.html', prediction=my_prediction)
+        return render_template('result.html', prediction=prediction_result)
         
-        
-
 if __name__ == '__main__':
 	app.run(debug=True)
